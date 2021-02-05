0bc3c1e394f183a0a2562f3f2dad410987884462
Mauricio Gomes <mauricio@edge14.com>
2021-02-05 15:02:02 -0500

    Add CLI::UI to all Xip CLI commands

--- lib/xip/commands/command.rb
+++ lib/xip/commands/command.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'xip'
+require 'cli/ui'
 
 module Xip
   module Commands
@@ -15,7 +16,20 @@ module Xip
       XIPRC = File.join(Dir.home, '.xiprc')
 
       def initialize(options)
+        CLI::UI::StdoutRouter.enable
+      end
 
+      private def cmd_puts(str)
+        puts CLI::UI.fmt(str)
+      end
+
+      private def load_xiprc
+        begin
+          file_contents = File.read(XIPRC)
+          YAML.load(file_contents)
+        rescue Errno::ENOENT
+          [] # .xiprc does not yet exist
+        end
       end
     end
   end
